c9c257776f0ae732a796f17aab3b011c46facf51
andrew.liden@federalmerchants.com <andrew.liden@federalmerchants.com>
2023-04-26 10:49:35 -0400

    Added a branch using the app directory template

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+const Layout = ({children}) => <html lang='en'>
+    <head>
+    </head>
+    <body>
+        {children}
+    </body>
+</html>;
+
+export default Layout;
---
+++ app/page.js
@@ -0,0 +1,19 @@
+const testRequest = async () => {
+    //You can perform any request here. Fetching nextjs's own documentation seemed a safe enough choice.
+    const r = await fetch('https://www.nextjs.org');
+    try {
+        r.clone();
+        return 'Cloned the response with no exception. You might not be running Node 16. You need to be on Node 16 to reproduce this bug.';
+    } catch (e) {
+        return `Cloning the response produced the following error: ${e.message}`;
+    }
+}
+
+const NodeSixteenCrash = async () => {
+    const text = await testRequest();
+    return <p>
+        {text}
+    </p>
+};
+
+export default NodeSixteenCrash;
